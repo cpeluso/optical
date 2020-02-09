c0925b4287a9c21f7605f45b98d28ef4e056d5c9
carlopeluso <cpelusoprivato@gmail.com>
2020-02-09 15:29:28 +0100

    Base Algorithm

--- CalendarOptimizer/Program.cs
+++ CalendarOptimizer/Program.cs
@@ -9,6 +9,7 @@ namespace CalendarOptimizer
     {
         private static List<Person> _people = new List<Person>();
         private static List<TimeSlot> _freeCommonIntervals = new List<TimeSlot>();
+        private static int _minLength = 30;
 
         static void Main(string[] args)
         {
@@ -25,19 +26,50 @@ namespace CalendarOptimizer
         {
             Person referencePerson = _people[0];
             _people.Remove(referencePerson);
+            _freeCommonIntervals = referencePerson.FreeIntervals;
 
             foreach(Person person in _people)
-            {
+                ComputeIntersections(person);
 
-                //... Do stuff
-                //Intersection Time Slot
-
-
-
-            }
 
         }
 
+        static void ComputeIntersections(Person sndPerson)
+        {
+            List<TimeSlot> currentCommonIntervals = new List<TimeSlot>();
+
+            foreach(TimeSlot freeInterval in _freeCommonIntervals)
+                foreach(TimeSlot personFreeInterval in sndPerson.FreeIntervals)
+                    if (CanBeIntersected(freeInterval, personFreeInterval))
+                        currentCommonIntervals.Add(GetIntersection(freeInterval, personFreeInterval));
+
+            _freeCommonIntervals = currentCommonIntervals;
+
+        }
+
+        static bool CanBeIntersected(TimeSlot fstTimeSlot, TimeSlot sndTimeSlot)
+        {
+            if (fstTimeSlot.EndTime < sndTimeSlot.StartTime || sndTimeSlot.EndTime < fstTimeSlot.StartTime) return false;
+            if (GetIntersection(fstTimeSlot, sndTimeSlot) == null) return false;
+            return true;
+        }
+
+        static TimeSlot GetIntersection(TimeSlot fstTimeSlot, TimeSlot sndTimeSlot)
+        {
+            TimeSlot timeSlot = new TimeSlot();
+
+            if (fstTimeSlot.StartTime > sndTimeSlot.StartTime) timeSlot.StartTime = fstTimeSlot.StartTime;
+            else timeSlot.StartTime = sndTimeSlot.StartTime;
+
+            if (fstTimeSlot.EndTime < sndTimeSlot.EndTime) timeSlot.EndTime = fstTimeSlot.EndTime;
+            else timeSlot.EndTime = sndTimeSlot.EndTime;
+
+            timeSlot.Length = timeSlot.EndTime - timeSlot.StartTime;
+
+            if (timeSlot.Length < _minLength) return null;
+
+            return timeSlot;
+        }
 
         //Just for having an input...
         static void LoadCalendars()
@@ -47,17 +79,17 @@ namespace CalendarOptimizer
 
             fst.AvailableFrom = 540;
             fst.AvailableTo = 1200;
-            fst.Meetings.Add(new TimeSlot ( 400, 460 ));
+            fst.Meetings.Add(new TimeSlot ( 400, 460 )); 
             fst.Meetings.Add(new TimeSlot ( 500, 610 ));
             fst.Meetings.Add(new TimeSlot ( 600, 660 ));
-            fst.Meetings.Add(new TimeSlot ( 630, 680 ));
-            fst.Meetings.Add(new TimeSlot ( 750, 870 ));
+            fst.Meetings.Add(new TimeSlot ( 630, 680 )); 
+            fst.Meetings.Add(new TimeSlot ( 750, 870 )); 
             fst.Meetings.Add(new TimeSlot ( 900, 930 ));
             fst.Meetings.Add(new TimeSlot ( 1080, 1140 ));
 
             snd.AvailableFrom = 540;
             snd.AvailableTo = 1200;
-            snd.Meetings.Add(new TimeSlot ( 540, 630 ));
+            snd.Meetings.Add(new TimeSlot ( 540, 630 )); 
             snd.Meetings.Add(new TimeSlot ( 720, 750 ));
             snd.Meetings.Add(new TimeSlot ( 780, 930 ));
             snd.Meetings.Add(new TimeSlot ( 960, 1020 ));
